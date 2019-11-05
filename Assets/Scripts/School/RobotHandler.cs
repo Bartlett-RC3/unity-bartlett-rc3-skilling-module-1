f485335cbdd5526e47f3e8d5703ca60714a7f27d
Octavian Gheorghiu <octavian.gheorghiu@yahoo.com>
2019-11-05 20:47:52 +0000

    Session 3
    We covered scripting logic and how to debug our scripts. Also made a cool robot!

---
+++ Assets/Scripts/School/RobotHandler.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotHandler : MonoBehaviour {
+
+    private bool up;
+    private bool move;
+
+	// Use this for initialization
+	void Start () {
+        up = false;
+        move = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // TIME
+        // Rotate Robot every 5 seconds and randomly move forward
+        if(Time.frameCount % 10 == 0 && move == true){
+            this.transform.RotateAround(transform.position, Vector3.up, Random.Range(-180, 180));
+            this.transform.Translate(Vector3.forward * Random.Range(1.0f, 5.0f), Space.Self);
+        }
+
+        // KEYBOARD INPUT
+        if(Input.GetKeyDown(KeyCode.Space)){
+            Transform rightHand = this.transform.GetChild(3);
+            if(up == false){
+                rightHand.Rotate(new Vector3(0, 0, 90), Space.Self);
+                up = true;
+            } else {
+                rightHand.Rotate(new Vector3(0, 0, -90), Space.Self);
+                up = false;
+            }
+        }
+
+        // MOUSE INPUT
+        if(Input.GetMouseButtonDown(0)){
+            move = !move;
+        }
+
+	}
+}
---
+++ Assets/Scripts/School/Session3_19.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Session3_19 : MonoBehaviour {
+
+    public GameObject robotPrefab;
+    public int noRobots = 10;
+
+	// Start function is run just once at the beginning of the game
+	void Start () {
+        // We will make randomly new robots in the scene
+        for (int i = 0; i < noRobots; i++){
+            //Make a random position and random rotation
+            Vector3 robotPos = new Vector3(Random.Range(-10, 10), 1, Random.Range(-10, 10));
+            Quaternion robotRot = Quaternion.identity;
+            GameObject newRobot = Instantiate(robotPrefab, robotPos, robotRot);
+            Transform robotHead = newRobot.transform.GetChild(0);
+            robotHead.GetComponent<MeshRenderer>().material.color = new Color(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f));
+        }
+	}
+	
+	// Update is called once per frame as fast as possible
+	void Update () {
+		
+	}
+}
